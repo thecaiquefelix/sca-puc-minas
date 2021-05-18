b1c98b5eda2befdb2825f1e988bd647ff2017bd2
Caique Gusmao <caiquefgusmao@gmail.com>
2021-05-17 22:54:59 -0300

    Implementado a consulta as monitorias

--- SCA.Gateway/SCA.Gateway/Controllers/MonitorController.cs
+++ SCA.Gateway/SCA.Gateway/Controllers/MonitorController.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SCA.Gateway.Domain;
+
 namespace SCA.Gateway.Controllers
 {
-    public class MonitorController
+    [ApiController]
+    [Route("api/v1/monitors")]
+    public class MonitorController : BaseController
     {
-        public MonitorController()
+        const string baseURL = "http://localhost:5005";
+
+        [HttpGet("dam/{damId:int}/metric/{metricId:int}")]
+        public async Task<IActionResult> GetAllAsync(int damId, int metricId)
         {
+            var url = $"{baseURL}/api/v1/monitors/dam/{damId:int}/metric/{metricId:int}";
+
+            return await GetListAsync<Monitor>(url);
         }
+
     }
 }
--- SCA.Gateway/SCA.Gateway/Domain/Monitor.cs
+++ SCA.Gateway/SCA.Gateway/Domain/Monitor.cs
@@ -3,8 +3,10 @@ namespace SCA.Gateway.Domain
 {
     public class Monitor
     {
-        public Monitor()
-        {
-        }
+        public int Id { get; set; }
+        public int DamId { get; set; }
+        public int MetricId { get; set; }
+        public DateTime Date { get; set; }
+        public int Value { get; set; }
     }
 }
