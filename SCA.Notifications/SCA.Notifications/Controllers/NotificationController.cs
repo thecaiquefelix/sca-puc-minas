da45e4ad00476e59d3c3df28ba84184051c13836
Caique Gusmao <caiquefgusmao@gmail.com>
2021-05-22 18:04:25 -0300

    Criado o projeto de notificacao

---
+++ SCA.Notifications/SCA.Notifications/Controllers/NotificationController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace SCA.Notifications.Controllers
+{
+    [Route("api/notifications")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<string> Send()
+        {
+            string accountSid = "";
+            string authToken = "";
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "",
+                from: new Twilio.Types.PhoneNumber(""),
+                to: new Twilio.Types.PhoneNumber("")
+            );
+
+            return "okay";
+        }
+    }
+}
